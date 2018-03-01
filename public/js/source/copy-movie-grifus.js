25471f20eb9d98da29aba1a760c235479f929283
Josantonius <hello@josantonius.com>
2018-03-01 04:41:30 +0100

    updated to version 1.0.3

--- public/js/source/copy-movie-grifus.js
+++ public/js/source/copy-movie-grifus.js
@@ -15,14 +15,20 @@ jQuery(document).ready(function($) {
 
       if ($("#s_right").length !== 0) { return 0; }
 
+      if (typeof eliasis !== 'undefined') {
+         var copy_movie_grifus = eliasis;
+      } else {
+         var copy_movie_grifus = copyMovieGrifus;
+      }
+
       $(".s_right").append(
          '<div id="grifusDataSheet" data-clipboard-text="" class="filtro_y">' +
-            '<h3>' + eliasis.copy_title + '</h3>' + 
+            '<h3>' + copy_movie_grifus.copy_title + '</h3>' + 
             '<ul class="scrolling mCustomScrollbar _mCS_2 mCS-autoHide mCS_no_scrollbar" style="position: relative; overflow: visible;">' +
                '<div id="mCSB_2" class="mCustomScrollBox mCS-minimal-dark mCSB_vertical mCSB_outside" tabindex="0" style="max-height: none;">' +
                   '<div id="clipboard-button" class="mCSB_container mCS_y_hidden mCS_no_scrollbar_y" style="position:relative; top:0; left:0;" dir="ltr">' +
                      '<li id="copyDataSheet" class="btn">' +
-                        '<a>' + eliasis.copy_button + '</a>' +
+                        '<a>' + copy_movie_grifus.copy_button + '</a>' +
                      '</li>' +
                   '</div>' +
                '</div>' +
@@ -36,7 +42,7 @@ jQuery(document).ready(function($) {
          '</div>'
       );
       
-      var movie = eliasis.movie_info;
+      var movie = copy_movie_grifus.movie_info;
       var clipboard = movie.the_title + movie.title + movie.published + movie.rated + movie.duration + movie.rating + movie.votes + movie.awards + movie.country + movie.sipnosis + movie.director + movie.stars + movie.cast + movie.genre;
       $("#grifusDataSheet").attr("data-clipboard-text", clipboard);
       new Clipboard("#grifusDataSheet");
